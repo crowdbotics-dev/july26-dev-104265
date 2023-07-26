babad5680b16a816ba0606173a8b197e3da94b87
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 19:45:46 +0000

    Update store.

--- store/julydevAPI/api.js
+++ store/julydevAPI/api.js
@@ -1,210 +1,132 @@
-import axios from "axios";
+import axios from "axios"
 const julydevAPI = axios.create({
   baseURL: "https://july26-dev-104265.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return julydevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return julydevAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_login_create(payload) {
-  return julydevAPI.post(`/api/v1/login/`, payload);
+  return julydevAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return julydevAPI.post(`/api/v1/signup/`, payload);
+  return julydevAPI.post(`/api/v1/signup/`, payload)
 }
-
-function modules_articles_article_list(payload) {
-  return julydevAPI.get(`/modules/articles/article/`);
-}
-
-function modules_articles_article_create(payload) {
-  return julydevAPI.post(`/modules/articles/article/`, payload);
-}
-
-function modules_articles_article_retrieve(payload) {
-  return julydevAPI.get(`/modules/articles/article/${payload.id}/`);
-}
-
-function modules_articles_article_update(payload) {
-  return julydevAPI.put(`/modules/articles/article/${payload.id}/`, payload);
-}
-
-function modules_articles_article_partial_update(payload) {
-  return julydevAPI.patch(`/modules/articles/article/${payload.id}/`, payload);
-}
-
-function modules_articles_article_destroy(payload) {
-  return julydevAPI.delete(`/modules/articles/article/${payload.id}/`);
-}
-
 function modules_files_uploads_list(payload) {
-  return julydevAPI.get(`/modules/files/uploads/`);
+  return julydevAPI.get(`/modules/files/uploads/`)
 }
-
 function modules_files_uploads_create(payload) {
-  return julydevAPI.post(`/modules/files/uploads/`, payload);
+  return julydevAPI.post(`/modules/files/uploads/`, payload)
 }
-
 function modules_files_uploads_retrieve(payload) {
-  return julydevAPI.get(`/modules/files/uploads/${payload.id}/`);
+  return julydevAPI.get(`/modules/files/uploads/${payload.id}/`)
 }
-
 function modules_files_uploads_update(payload) {
-  return julydevAPI.put(`/modules/files/uploads/${payload.id}/`, payload);
+  return julydevAPI.put(`/modules/files/uploads/${payload.id}/`, payload)
 }
-
 function modules_files_uploads_partial_update(payload) {
-  return julydevAPI.patch(`/modules/files/uploads/${payload.id}/`, payload);
+  return julydevAPI.patch(`/modules/files/uploads/${payload.id}/`, payload)
 }
-
 function modules_files_uploads_destroy(payload) {
-  return julydevAPI.delete(`/modules/files/uploads/${payload.id}/`);
+  return julydevAPI.delete(`/modules/files/uploads/${payload.id}/`)
 }
-
 function modules_hubspot_service_access_token_create(payload) {
-  return julydevAPI.post(`/modules/hubspot/service/access/token/`);
+  return julydevAPI.post(`/modules/hubspot/service/access/token/`)
 }
-
 function modules_hubspot_service_contact_deals_list_retrieve(payload) {
-  return julydevAPI.get(`/modules/hubspot/service/contact/deals/list/`);
+  return julydevAPI.get(`/modules/hubspot/service/contact/deals/list/`)
 }
-
 function modules_hubspot_service_deals_associations_create_create(payload) {
-  return julydevAPI.post(`/modules/hubspot/service/deals/associations/create/`);
+  return julydevAPI.post(`/modules/hubspot/service/deals/associations/create/`)
 }
-
 function modules_hubspot_service_deals_create_create(payload) {
-  return julydevAPI.post(`/modules/hubspot/service/deals/create/`);
+  return julydevAPI.post(`/modules/hubspot/service/deals/create/`)
 }
-
 function modules_hubspot_service_deals_list_retrieve(payload) {
-  return julydevAPI.get(`/modules/hubspot/service/deals/list/`);
+  return julydevAPI.get(`/modules/hubspot/service/deals/list/`)
 }
-
 function modules_hubspot_service_deals_remove_destroy(payload) {
-  return julydevAPI.delete(`/modules/hubspot/service/deals/remove/`);
+  return julydevAPI.delete(`/modules/hubspot/service/deals/remove/`)
 }
-
 function modules_hubspot_service_deals_single_retrieve(payload) {
-  return julydevAPI.get(`/modules/hubspot/service/deals/single/`);
+  return julydevAPI.get(`/modules/hubspot/service/deals/single/`)
 }
-
 function modules_hubspot_service_events_create_create(payload) {
-  return julydevAPI.post(`/modules/hubspot/service/events/create/`);
+  return julydevAPI.post(`/modules/hubspot/service/events/create/`)
 }
-
 function modules_hubspot_service_meeting_contacts_list_retrieve(payload) {
-  return julydevAPI.get(`/modules/hubspot/service/meeting/contacts/list/`);
+  return julydevAPI.get(`/modules/hubspot/service/meeting/contacts/list/`)
 }
-
 function modules_hubspot_service_ticket_associations_create_update(payload) {
-  return julydevAPI.put(`/modules/hubspot/service/ticket/associations/create/`);
+  return julydevAPI.put(`/modules/hubspot/service/ticket/associations/create/`)
 }
-
 function modules_hubspot_service_ticket_associations_list_retrieve(payload) {
-  return julydevAPI.get(`/modules/hubspot/service/ticket/associations/list/`);
+  return julydevAPI.get(`/modules/hubspot/service/ticket/associations/list/`)
 }
-
 function modules_hubspot_service_tickets_create_create(payload) {
-  return julydevAPI.post(`/modules/hubspot/service/tickets/create/`);
+  return julydevAPI.post(`/modules/hubspot/service/tickets/create/`)
 }
-
 function modules_hubspot_service_tickets_list_retrieve(payload) {
-  return julydevAPI.get(`/modules/hubspot/service/tickets/list/`);
+  return julydevAPI.get(`/modules/hubspot/service/tickets/list/`)
 }
-
 function modules_hubspot_service_tickets_remove_destroy(payload) {
-  return julydevAPI.delete(`/modules/hubspot/service/tickets/remove/`);
+  return julydevAPI.delete(`/modules/hubspot/service/tickets/remove/`)
 }
-
 function modules_hubspot_service_tickets_single_retrieve(payload) {
-  return julydevAPI.get(`/modules/hubspot/service/tickets/single/`);
+  return julydevAPI.get(`/modules/hubspot/service/tickets/single/`)
 }
-
 function modules_hubspot_service_webhook_create(payload) {
-  return julydevAPI.post(`/modules/hubspot/service/webhook/`);
+  return julydevAPI.post(`/modules/hubspot/service/webhook/`)
 }
-
 function modules_zoominfo_auth_token_create(payload) {
-  return julydevAPI.post(`/modules/zoominfo/auth/token/`);
+  return julydevAPI.post(`/modules/zoominfo/auth/token/`)
 }
-
 function modules_zoominfo_data_bulk_create(payload) {
-  return julydevAPI.post(`/modules/zoominfo/data/bulk/`);
+  return julydevAPI.post(`/modules/zoominfo/data/bulk/`)
 }
-
 function modules_zoominfo_data_enrich_create(payload) {
-  return julydevAPI.post(`/modules/zoominfo/data/enrich/`);
+  return julydevAPI.post(`/modules/zoominfo/data/enrich/`)
 }
-
 function modules_zoominfo_data_search_create(payload) {
-  return julydevAPI.post(`/modules/zoominfo/data/search/`);
+  return julydevAPI.post(`/modules/zoominfo/data/search/`)
 }
-
 function rest_auth_login_create(payload) {
-  return julydevAPI.post(`/rest-auth/login/`, payload);
+  return julydevAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return julydevAPI.get(`/rest-auth/logout/`);
+  return julydevAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return julydevAPI.post(`/rest-auth/logout/`);
+  return julydevAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return julydevAPI.post(`/rest-auth/password/change/`, payload);
+  return julydevAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return julydevAPI.post(`/rest-auth/password/reset/`, payload);
+  return julydevAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return julydevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return julydevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return julydevAPI.post(`/rest-auth/registration/`, payload);
+  return julydevAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return julydevAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return julydevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return julydevAPI.get(`/rest-auth/user/`);
+  return julydevAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return julydevAPI.put(`/rest-auth/user/`, payload);
+  return julydevAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return julydevAPI.patch(`/rest-auth/user/`, payload);
+  return julydevAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
-  modules_articles_article_list,
-  modules_articles_article_create,
-  modules_articles_article_retrieve,
-  modules_articles_article_update,
-  modules_articles_article_partial_update,
-  modules_articles_article_destroy,
   modules_files_uploads_list,
   modules_files_uploads_create,
   modules_files_uploads_retrieve,
@@ -242,4 +164,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
