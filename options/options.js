d2f485af353f29d7d22577507cd412bb5889a613
Hugo Seabra <hugo.seabra@crowdbotics.com>
2023-07-26 16:31:56 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "july26-dev-104265",
-  url: "https://july26-dev-104265.botics.co",
-  api: "https://july26-dev-104265.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "july26-dev-104265",
+  "url": "https://july26-dev-104265.botics.co",
+  "api": "https://july26-dev-104265.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
